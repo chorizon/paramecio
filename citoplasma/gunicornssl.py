967fb8429ef9ec46101a035407c648c99cf00f09
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-09-17 20:16:32 +0200

    Fix on gunicornssl

--- citoplasma/gunicornssl.py
+++ citoplasma/gunicornssl.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# This module is suitable for create ssl apis that don't need much performance, if you need more performance use nginx or apache proxiying for generate https content and Paramecio with gunicorn or others wsgi servers for generate the html/json content.
+
 from bottle import ServerAdapter
 import ssl
 
