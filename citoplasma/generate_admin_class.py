434b4b33e880765af0f2973ef586ccafa7e0f10a
Antonio de la Rosa <webmaster@web-t-sys.com>
2015-11-26 02:28:26 +0100

    Added generate admin class source file

---
+++ citoplasma/generate_admin_class.py
@@ -0,0 +1,32 @@
+from citoplasma import list
+
+class GenerateAdminClass:
+    
+    def __init__(model):
+        self.model_name=''
+
+        self.list
+        
+        #For the future
+        
+        self.arr_fields_insert={}
+        
+        self.arr_fields_edit={}
+        
+        self.url=''
+        
+        self.safe=0;
+        
+        self.arr_links={}
+        
+        self.hierarchy
+        
+        self.text_add_item=''
+        
+        self.no_insert=0
+        
+        self.no_delete=0
+
+
+    def show(op=0):
+        pass
